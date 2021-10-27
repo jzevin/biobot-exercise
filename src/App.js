dde7f9728ca70d5f4b91b3e7a14d098cc6b14bdd
Jeremy Zevin <jeremy.zevin@gmail.com>
2021-10-27 14:59:46 -0400

    jz | app changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import KitSearch from "./KitSearch.js";
+import KitSearchDetails from "./KitSearchDetails.js";
 
 function App() {
+  const [searchResultsDetail, setSearchResultsDetail] = useState({}),
+    [appUiState, setAppUiState] = useState({ autoCompleteIsOpen: false });
+
+  function onClickSearchItem(itemData) {
+    //context would be better for ui state but fine for this exercise
+    setAppUiState({ ...appUiState, autoCompleteIsOpen: false });
+    setSearchResultsDetail(itemData); //set the search results detail
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="biobot logo" />
       </header>
+
+      <section id="content">
+        <KitSearch
+          onAppClickSearchItem={onClickSearchItem}
+          appUiState={appUiState}
+          setAppUiState={setAppUiState}
+        />
+        <KitSearchDetails kitData={searchResultsDetail} />
+      </section>
     </div>
   );
 }
