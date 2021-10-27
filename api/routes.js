8a6d04b1a4cef7daa063ec1afc404611b6ca05aa
Jeremy Zevin <jeremy.zevin@gmail.com>
2021-10-27 14:59:11 -0400

    jz | route changes

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,21 @@
-export async function routes (fastify, options) {
-  fastify.get('/search/:kitId', async (request, reply) => {
-    // this filters any kits that include the passed search string and all if empty
-    const filtered = global.kitShippingData.filter(el => {
+export async function routes(fastify, options) {
+  fastify.get("/kits/search/:searchToken", async (request, reply) => {
+    // for scalability we obviously don't want to return 1000+ results
+    // every time, so this filters any kits that include the passed search
+    const filtered = global.kitShippingData.filter((el) => {
+      const { searchToken } = request.params;
+      return el.label_id.includes(searchToken);
+    });
+    return filtered;
+  });
+  fastify.get("/kits", async (request, reply) => {
+    return global.kitShippingData;
+  });
+  fastify.get("/kits/:kitId", async (request, reply) => {
+    const found = global.kitShippingData.find((el) => {
       const { kitId } = request.params;
-      return el.label_id.includes(kitId);
-    })
-    return { data: filtered };
-  })
+      return el.id === parseInt(kitId);
+    });
+    return found || { error: "not found", params: request.params };
+  });
 }
-
